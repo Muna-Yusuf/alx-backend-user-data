f5b9dc281f132589f2ecc24b504765189290a58b
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-07-19 22:26:55 +0000

    Find user by session ID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Auth Models."""
-
+"""Auth Models."""
 from bcrypt import hashpw, gensalt, checkpw
 import bcrypt
 from db import DB
@@ -18,18 +17,18 @@ def _hash_password(password: str) -> bytes:
 
 
 def _generate_uuid() -> str:
-    """Generates Uuid."""
+    """Generates UUID."""
     return str(uuid4())
 
 
 class Auth:
-    """Class to interac with the authentication."""
+    """Class to interact with the authentication."""
 
     def __init__(self):
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """Class to regiester a user."""
+        """Registers a user."""
         try:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
@@ -37,34 +36,35 @@ class Auth:
             return self._db.add_user(email, _hash_password(password))
 
     def valid_login(self, email: str, password: str) -> bool:
-        """Valid Login Of User."""
+        """Validates login of user."""
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
             return False
-        return checkpw(password.encode('utf-8'), user.hash_password)
+        return checkpw(password.encode('utf-8'), user.hashed_password)
 
     def create_session(self, email: str) -> str:
-        """Create a New Session"""
+        """Creates a new session."""
         try:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
+            return session_id
         except NoResultFound:
-            return
+            return None
 
     def get_user_from_session_id(self, session_id: str) -> str:
-        """Gets User From Session Id."""
+        """Gets user from session ID."""
         if session_id is None:
-            return
+            return None
         try:
             user = self._db.find_user_by(session_id=session_id)
             return user.email
         except NoResultFound:
-            return
+            return None
 
     def destroy_session(self, user_id: int) -> None:
-        """Destroy Session."""
+        """Destroys session."""
         try:
             user = self._db.find_user_by(id=user_id)
             self._db.update_user(user.id, session_id=None)
@@ -72,21 +72,21 @@ class Auth:
             pass
 
     def get_reset_password_token(self, email: str) -> str:
-        """Get Reset Password Token."""
+        """Gets reset password token."""
         try:
             user = self._db.find_user_by(email=email)
             reset_token = _generate_uuid()
             self._db.update_user(user.id, reset_token=reset_token)
             return reset_token
         except NoResultFound:
-            raise ValueError
+            raise ValueError("User not found")
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """Updates Password."""
+        """Updates password."""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             self._db.update_user(user.id,
                                  hashed_password=_hash_password(password),
                                  reset_token=None)
         except NoResultFound:
-            raise ValueError
+            raise ValueError("Invalid reset token")
