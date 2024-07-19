9a3641a61a4179c713ad2fef15fd5503a04fef71
Muna-Yusuf <mun.y.a.jim@gmail.com>
2024-07-19 22:44:46 +0000

    fixing app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,19 +15,19 @@ def hello() -> str:
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def user() -> str:
-    """POST route for user register & Returns messege"""
+    """POST route for user register & Returns message."""
     email = request.form.get('email')
     password = request.form.get('password')
     try:
         AUTH.register_user(email, password)
         return jsonify({"email": f"{email}", "message": "user created"}), 200
     except Exception:
-        return jsonify({"messege": "email already registered"}), 400
+        return jsonify({"message": "email already registered"}), 400
 
 
 @app.route('/sessions', methods=['POST'], strict_slashes=False)
 def login() -> str:
-    """Login & Returns messege."""
+    """Login & Returns message."""
     email = request.form.get('email')
     password = request.form.get('password')
     valid_login = AUTH.valid_login(email, password)
@@ -53,20 +53,20 @@ def logout() -> str:
 
 @app.route('/profile', methods=['GET'], strict_slashes=False)
 def profile() -> str:
-    """profile & Return message."""
+    """Profile & Return message."""
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        return jsonify({"email": user.email}), 200
+        return jsonify({"email": user}), 200
     else:
         abort(403)
 
 
 @app.route('/reset_password', methods=['POST'], strict_slashes=False)
 def get_reset_password_token() -> str:
-    """get_reset_password & Return message."""
+    """Get reset password token & Return message."""
     email = request.form.get('email')
-    user = AUTH.create_session(email)
+    user = AUTH.get_user_from_session_id(email)
     if not user:
         abort(403)
     else:
@@ -76,14 +76,16 @@ def get_reset_password_token() -> str:
 
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
-    """update_password & Return message."""
+    """Update password & Return message."""
     email = request.form.get('email')
     reset_token = request.form.get('reset_token')
     new_psw = request.form.get('new_password')
     try:
         AUTH.update_password(reset_token, new_psw)
-        return jsonify({"email": f"{email}",
-                        "message": "Password updated"}), 200
+        return jsonify({
+            "email": f"{email}",
+            "message": "Password updated"
+        }), 200
     except Exception:
         abort(403)
 
